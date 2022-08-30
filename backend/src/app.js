7bbcb741fb27f8b0c8ba59c890fe0653f1a4cef9
migromarj <miguelromeroarjona@gmail.com>
2022-08-30 19:01:32 +0200

    Finished backend set up

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,21 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+import {options} from "./swaggerOptions";
+
+import tasksRoutes from "./routes/tasks";
+
+const specs = swaggerJSDoc(options)
 
 const app = express()
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(tasksRoutes)
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs))
 
 export default app
